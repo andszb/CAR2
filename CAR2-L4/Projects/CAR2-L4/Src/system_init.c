d287125756dd266cd7c4a0a65861b91cc72252cd
ladanyiz <ladzoltan@gmail.com>
2017-09-12 08:57:29 +0200

    arranging pin inits

--- CAR2-L4/Projects/CAR2-L4/Src/system_init.c
+++ CAR2-L4/Projects/CAR2-L4/Src/system_init.c
@@ -20,6 +20,7 @@ int8_t system_init();
 int8_t pin_init();
 int8_t portA_init();
 int8_t portB_init();
+int8_t portD_init();
 
 //call init functions
 int8_t system_init()
@@ -42,79 +43,85 @@ int8_t pin_init()
 // init port A pins
 int8_t portA_init()
 {
-	GPIO_InitTypeDef GPIO_Init;
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 
+	GPIO_InitDef.Speed = GPIO_SPEED_FAST;
+
 	//init D4 (PA3) pin EXTI mode
-	GPIO_Init.Speed = GPIO_SPEED_FAST;
-	GPIO_Init.Mode = GPIO_MODE_IT_RISING_FALLING;
-	GPIO_Init.Pull = GPIO_PULLUP;
-	GPIO_Init.Pin = GPIO_PIN_3;
-	HAL_GPIO_Init(GPIOA, &GPIO_Init);
+	GPIO_InitDef.Pin = GPIO_PIN_3;
+	GPIO_InitDef.Mode = GPIO_MODE_IT_RISING_FALLING;
+	GPIO_InitDef.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitDef);
 
 #ifdef DEBUG_MODE
 	printf("Proxim EXTI pin init done.\n");
 #endif
-	__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	// Initialize pin D9 (PA15) as PWM (TIM2) output
+	GPIO_InitDef.Pin = GPIO_PIN_15;
 	GPIO_InitDef.Mode = GPIO_MODE_AF_PP;
 	GPIO_InitDef.Pull = GPIO_PULLDOWN;
-	GPIO_InitDef.Speed = GPIO_SPEED_MEDIUM;
-	GPIO_InitDef.Pin = GPIO_PIN_15;
 	GPIO_InitDef.Alternate = GPIO_AF1_TIM2;
 
+#ifdef DEBUG_MODE
+	printf("Motor PWM pin init done.\n");
+#endif
+
+	//Initialize D11 and D12 (PA6 and PA7) as LED output
+	GPIO_InitDef.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+	GPIO_InitDef.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitDef.Pull = GPIO_NOPULL;
+
+#ifdef DEBUG_MODE
+	printf("Proxim LED init done.\n");
+#endif
+
 	HAL_GPIO_Init(GPIOA, &GPIO_InitDef);
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
 
 	return 0;
 }
 
-	//Initialize D11 and D12 (PA6 and PA7) as LED output
-	GPIO_InitTypeDef GPIO_Init;
-	GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_Init.Pull = GPIO_NOPULL;
-	GPIO_Init.Speed = GPIO_SPEED_FAST;
-	GPIO_Init.Pin = GPIO_PIN_6 | GPIO_PIN_7;
-	HAL_GPIO_Init(GPIOA, &GPIO_Init);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
-#ifdef DEBUG_MODE
-	printf("Proxim LED init done.\n");
-#endif
-	return 0;
 // init port B pins
 int8_t portB_init()
 {
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-	// Initialize pin D5 (PB4) as PWM (TIM3) output
-	GPIO_InitDef.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitDef.Pull = GPIO_PULLDOWN;
-	GPIO_InitDef.Speed = GPIO_SPEED_MEDIUM;
-	GPIO_InitDef.Pin = GPIO_PIN_4;
-	GPIO_InitDef.Alternate = GPIO_AF2_TIM3;
-
-	HAL_GPIO_Init(GPIOB, &GPIO_InitDef);
-}
-
-// init portB pins
-int8_t portB_init()
-{
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+	GPIO_InitDef.Speed = GPIO_SPEED_FAST;
 
 	//init D3 (PB0) as trigger for proximity sensors
-	GPIO_InitTypeDef GPIO_Init;
-	GPIO_Init.Pin = GPIO_PIN_0;
-	GPIO_Init.Speed = GPIO_SPEED_FAST;
-	GPIO_Init.Pull = GPIO_PULLUP;
-	GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
-	HAL_GPIO_Init(GPIOB, &GPIO_Init);
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	GPIO_InitDef.Pin = GPIO_PIN_0;
+	GPIO_InitDef.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitDef.Pull = GPIO_PULLUP;
+
 #ifdef DEBUG_MODE
 	printf("Proxim trigger1 init done.\n");
 #endif
 
+	// Initialize pin D5 (PB4) as PWM (TIM3) output
+	GPIO_InitDef.Pin = GPIO_PIN_4;
+	GPIO_InitDef.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitDef.Pull = GPIO_PULLDOWN;
+	GPIO_InitDef.Alternate = GPIO_AF2_TIM3;
 
+#ifdef DEBUG_MODE
+	printf("Servo PWM pin init done.\n");
+#endif
+
+	// Initialize pin D6 and D8 (PB1 and PB2) as output for motor direction control
+	GPIO_InitDef.Pin = GPIO_PIN_1 | GPIO_PIN_2;
+	GPIO_InitDef.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitDef.Pull = GPIO_NOPULL;
+
+#ifdef DEBUG_MODE
+	printf("Motor direction pin init done.\n");
+#endif
+
+	HAL_GPIO_Init(GPIOB, &GPIO_InitDef);
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
 
 	return 0;
 }
@@ -123,16 +130,20 @@ int8_t portD_init()
 {
 	__HAL_RCC_GPIOD_CLK_ENABLE();
 
+	GPIO_InitDef.Speed = GPIO_SPEED_FAST;
+
 	//init D2 (PD14) as trigger for proximity sensors
-	GPIO_InitTypeDef GPIO_Init;
-	GPIO_Init.Pin = GPIO_PIN_14;
-	GPIO_Init.Speed = GPIO_SPEED_FAST;
-	GPIO_Init.Pull = GPIO_PULLUP;
-	GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
-	HAL_GPIO_Init(GPIOD, &GPIO_Init);
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+	GPIO_InitDef.Pin = GPIO_PIN_14;
+	GPIO_InitDef.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitDef.Pull = GPIO_PULLUP;
+
 #ifdef DEBUG_MODE
 	printf("Proxim trigger2 init done.\n");
 #endif
+
+	HAL_GPIO_Init(GPIOD, &GPIO_InitDef);
+
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+
 	return 0;
 }
