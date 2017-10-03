e8e7884e22a2ba7b5f6e1d34a8e0bfb1cb90d6a6
andszb <andiszabo22@gmail.com>
2017-10-03 17:14:38 +0200

    fine tune pd_control for line sensor system

--- CAR2-L4/Projects/CAR2-L4/Src/servo_control.c
+++ CAR2-L4/Projects/CAR2-L4/Src/servo_control.c
@@ -4,10 +4,11 @@
 
 #define LINE_DETECT_DELAY 10
 
+
 const float ctrler_out_min = -36.0;
 const float ctrler_out_max = 36.0;
-float p_value = 0.1;
-float d_value = 0.1;
+float p_value = 1.0;
+float d_value = 1.0;
 int16_t angle_error = 0;
 int16_t prev_error = 0;
 int16_t difference = 0;
@@ -15,14 +16,14 @@ float ctrler_out = 0.0;
 int8_t last_line_position = 0;
 
 void servo_pwm_set_duty(float duty);
-void set_servo_angle()
+/*void set_servo_angle()
 {
-/*
+ *
  *	5% duty cycle is the leftmost position of the steering, 10% is the rightmost.
  *	If we say leftmost is -36 degrees, rightmost is 36,
  *	then 1 degree equals to (5 / 72) % in duty cycle.
  *	7.5 % is 0 degrees.
- */
+ *
 	float angle = pd_control();
 	float duty = 7.5 + ((5.0 / 72.0) * (float)angle);
 	servo_pwm_set_duty(duty);
@@ -30,6 +31,7 @@ void set_servo_angle()
 	printf("angle: %.2f, duty: %.2f; ", angle, duty);
 #endif
 }
+*/
 
 void servo_pwm_set_duty(float duty)
 {
@@ -40,10 +42,10 @@ void servo_pwm_set_duty(float duty)
 	return;
 }
 
-float pd_control()
+float pd_control(int8_t line_position_tmp)
 {
 	//convert line position into angle: from middle position one sensor left or right means 36/4 = 9 degree error
-	angle_error = line_position * 9;
+	angle_error = line_position_tmp * 9;
 	difference = angle_error - prev_error;
 	ctrler_out = p_value * (float)angle_error + d_value * (float)difference;
 	if (ctrler_out < ctrler_out_min) {
@@ -53,19 +55,27 @@ float pd_control()
 		ctrler_out = ctrler_out_max;
 	}
 	prev_error = angle_error;
-
+#ifdef DEBUG_MODE
+	printf("pd ctrler out angle: %.2f\n; ", ctrler_out);
+#endif
 	return ctrler_out;
 }
 
 //turn servo based on line position
-void turn_servo()
+void turn_servo(int8_t line_position_tmp)
 {
+	float angle;
+	float duty;
 	if (no_line_flag == 0){
 		//If line position is detected, turn servo
-		set_servo_angle();
-
+		angle = pd_control(line_position_tmp);
+		duty = 7.5 + (5.0 / 72.0 * angle);
+		servo_pwm_set_duty(duty);
+#ifdef DEBUG_MODE
+		printf("angle: %.2f, duty: %.2f; ", angle, duty);
+#endif
 		//save last line position
-		last_line_position = line_position;
+		last_line_position = line_position_tmp;
 
 	} else if(no_line_flag == 1){
 		//If no line is detected
@@ -73,23 +83,39 @@ void turn_servo()
 			//and the last line position was sensor #1
 			//keep turning 'LINE_DETECT_DELAY' times
 			if (last_line_position == 4){
-				float duty = 7.5 + ((5.0 / 72.0) * 36.0);
+				angle = -36.0;
+				duty = 7.5 + ((5.0 / 72.0) * angle);
 				servo_pwm_set_duty(duty);
+#ifdef DEBUG_MODE
+		printf("angle: %.2f, duty: %.2f; ", angle, duty);
+#endif
 			} else if (last_line_position == -4){
 				//and the last line position was sensor #9
 				//keep turning 'LINE_DETECT_DELAY' times
-				float duty = 7.5 + ((5.0 / 72.0) * -36.0);
+				angle = 36.0;
+				duty = 7.5 + ((5.0 / 72.0) * angle);
+#ifdef DEBUG_MODE
+		printf("angle: %.2f, duty: %.2f; ", angle, duty);
+#endif
 				servo_pwm_set_duty(duty);
 			}
 		}
 		//if there is still no line detected, try to turn to the opposite direction
 		for (int8_t i = 0; i < LINE_DETECT_DELAY; i++){
 			if (last_line_position == 4){
-				float duty = 7.5 + ((5.0 / 72.0) * -36.0);
+				angle = 36.0;
+				duty = 7.5 + ((5.0 / 72.0) * angle);
 				servo_pwm_set_duty(duty);
+#ifdef DEBUG_MODE
+		printf("angle: %.2f, duty: %.2f; ", angle, duty);
+#endif
 			} else if (last_line_position == -4){
-				float duty = 7.5 + ((5.0 / 72.0) * 36.0);
+				angle = -36.0;
+				duty = 7.5 + ((5.0 / 72.0) * angle);
 				servo_pwm_set_duty(duty);
+#ifdef DEBUG_MODE
+		printf("angle: %.2f, duty: %.2f; ", angle, duty);
+#endif
 			}
 		}
 		//if there is no line found, stop the car
